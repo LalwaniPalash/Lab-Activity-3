4e582afc4fd45d922246c5a8b32d6fd94e6b221b
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-09 11:15:19 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,23 @@
+import time
+
+print("Program to swap values of 2 variables!\n")
+time.sleep(2)
+
+a = None
+b = None
+
+while a is None and b is None:
+	try:
+		a = int(input("Value of a: "))
+		b = int(input("Value of b: "))
+	except ValueError:
+		input("Invalid Input!")
+
+swap = input("Do you want to swap the values?(y/n): ")
+
+if swap == "y":
+	a, b = b, a
+else: 
+	None
+
+print(f"Value of A = {a} and Value of B = {b}")
