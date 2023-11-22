47166df87b3542675297c02a32627c282f2f8f01
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-22 15:56:36 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,19 @@
+import time
+import random
+import math
+
+print("\nProgram to truncated, floored and celled value of [-0.2, -0.5, 0.2, 1.5, 2.9]!\n")
+time.sleep(2)
+
+numbers = [-2.8, -0.5, 0.2, 1.5, 2.9]
+
+for num in numbers:
+    truncated = math.trunc(num)
+    floored = math.floor(num)
+    ceiled = math.ceil(num)
+    
+    print(f"Original: {num}")
+    print(f"Truncated: {truncated}")
+    print(f"Floored: {floored}")
+    print(f"Celed: {ceiled}")
+    print()
