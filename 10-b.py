3ab1ce5502aea9f3e79fb684c1230dfeeb233afc
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 11:24:42 +0530

    Update 10-b.py

--- 10-b.py
+++ 10-b.py
@@ -3,11 +3,9 @@ import time
 print("\nProgram to convert a float value to numeric string!\n")
 time.sleep(2)
 
-# Take user input or use default float value
 user_input = input("Enter a float value to be converted to numeric string (press Enter for default[4.33]: ")
 float_value = float(user_input) if user_input else 4.33
 
-# Convert the float to a numeric string
 numeric_string = str(float_value)
 
 print(f"The numeric string equivalent is: {numeric_string}")
