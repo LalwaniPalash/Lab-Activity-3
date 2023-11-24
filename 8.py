2ea5e426ef7557b0fc2b41dc53dffc4299af5f66
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 10:32:53 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,12 @@
+import time
+import math
+
+print("\nProgram to evaluate expressions!\n")
+time.sleep(2)
+
+print(2**6 // 8%2)
+print(9**2 // 5-3)
+print(10 + 6 - 2 % 3 + 7 - 2)
+print(5 % 10 + 10 - 23 * 4 // 3)
+print(5 + 5 // 5 - 5 * 5 ** 5 % 5)
+print(7 % 7 + 7 // 7 - 7 * 7)
