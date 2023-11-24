c9d9ede0937a646ebe941c80b3f7f60e82c71178
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 10:33:18 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,11 @@
+import time
+import math
+
+print("\nProgram to evaluate expressions!\n")
+time.sleep(2)
+
+print(min(2,6,8,5))
+print(bin(46))
+print(round(10.544336, 2))
+print(math.hypot(6, 8))
+print(math.modf(3.1415))
