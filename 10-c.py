8a4cb4d9fde3f04d4dc8ae469a5dcc377271a44c
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-11-24 11:27:47 +0530

    Create 10-c.py

---
+++ 10-c.py
@@ -0,0 +1,13 @@
+import time
+
+print("\nProgram to obtain integer quotient and remainder while dividing 29 by 5!\n")
+time.sleep(2)
+
+dividend = 29
+divisor = 5
+
+quotient = dividend // divisor
+remainder = dividend % divisor
+
+print(f"The integer quotient is: {quotient}")
+print(f"The remainder is: {remainder}")
